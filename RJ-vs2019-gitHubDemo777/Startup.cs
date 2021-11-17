534c8c6b0bf61c3b9bf674c6b4bfb0d698cee0a5
shivakumar <sk625700@gmail.com>
2021-11-17 11:09:56 +0530

    Program Edited

--- RJ-vs2019-gitHubDemo777/Startup.cs
+++ RJ-vs2019-gitHubDemo777/Startup.cs
@@ -33,6 +33,7 @@ namespace RJ_vs2019_gitHubDemo777
                 endpoints.MapGet("/", async context =>
                 {
                     await context.Response.WriteAsync("Hello World!");
+                    await context.Response.WriteAsync("  <<<<Hello  SHIVAA!>>>");
                 });
             });
         }
